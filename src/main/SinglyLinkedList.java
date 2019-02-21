491e6758d87c56e49f1f6934335e45c902cb9ee5
gnishtha <nishtha.gupta_ccv17@gla.ac.in>
2019-02-21 08:29:05 +0530

    added constructor

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -2,6 +2,17 @@ package main;
 
 public class SinglyLinkedList {
     private static class Node{
+        private int data;
+        private Node next;
 
+        public Node ( int data ) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node ( int data , Node next ) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
